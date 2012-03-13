ce1d380c84856c3a3ab1c750c1ea4f673d7dbcf2
AJ ONeal <coolaj86@gmail.com>
2012-03-13 17:28:42 -0600

    create the web-client folder before using it

--- browser/build.sh
+++ browser/build.sh
@@ -1,10 +1,10 @@
 PUBLICTARGET="../web-client/"
 
+cp -R static/ ${PUBLICTARGET}/
 pakmanager build
 jade *.jade
 lessc *.less > style.css
 mv *.css ${PUBLICTARGET}/
 mv *.html ${PUBLICTARGET}/
 mv pakmanaged.js ${PUBLICTARGET}/appsnap.js
-cp -R static/ ${PUBLICTARGET}/
 #cp -R ./public/ ./windows/
