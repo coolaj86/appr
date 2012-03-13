44e877d65fb98ae97a211e866130a777f17403e9
AJ ONeal <coolaj86@gmail.com>
2012-03-13 17:47:28 -0600

    build scripts adjusting and working (or so we think)

---
+++ README.md
@@ -0,0 +1,27 @@
+browser
+===
+
+The client that runs in the browser.
+
+To build for deployment:
+
+    cd browser
+    ./build.sh
+    # creates ../web-client
+
+This is packaged into the `web-client` directory and served up server as well as distributed with the client.
+
+client
+===
+
+The webserver that runs on the client computer
+
+To build for deployment:
+
+    ???
+    # copies web-client
+
+server
+===
+
+The application hoster distributor
--- browser/build.sh
+++ browser/build.sh
@@ -1,10 +1,15 @@
 PUBLICTARGET="../web-client/"
 
 cp -R static/ ${PUBLICTARGET}/
-pakmanager build
-jade *.jade
+echo "Compiling JavaScript to CommonJS"
+pakmanager build > /dev/null 2> /dev/null
+echo "Compiling Jade to HTML"
+jade *.jade > /dev/null
+echo "Compiling LESS to CSS"
 lessc *.less > style.css
+echo -n "Moving packaged files..."
 mv *.css ${PUBLICTARGET}/
 mv *.html ${PUBLICTARGET}/
 mv pakmanaged.js ${PUBLICTARGET}/appsnap.js
 #cp -R ./public/ ./windows/
+echo " done"
