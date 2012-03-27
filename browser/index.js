a9e1b02d2fc16a149d9989bcf02bfce603554ebe
Eric Jacobs <eric@chomp.us>
2012-03-27 17:46:53 -0600

    Fixed some junk with the apps loading

--- browser/index.js
+++ browser/index.js
@@ -1,6 +1,12 @@
 (function() {
   "use strict";
 
+  /*
+   *
+   * Variable declaration
+   *
+   */
+
   var $ = require("ender")
     , request = require("ahr2")
     , forEachAsync = require("forEachAsync")
@@ -11,33 +17,11 @@
     , headerHeight = 99
     ;
 
-  function populateLists() {
-    $('#js-applists').hide();
-    sequence
-      .then(function(next) {
-        request.get("http://localhost:" + port + "/installed").when( function(err, ahr, data) {
-          data = checkResponse(err, data);
-          if(!data) { console.error("bad data!"); return; }
-          appendList('.js-currently-installed', data.data);
-          next(data.data);
-        });
-      })
-      .then(function(next, alreadyInstalled) {
-        var availableApps
-          ;
-        request.get("http://localhost:" + port + "/applist").when( function(err, ahr, data) {
-          data = checkResponse(err, data);
-          if(!data) { console.error("bad data!"); return; }
-          availableApps = _.difference(data, alreadyInstalled);
-          appendList('.js-app-list', availableApps, ["js-available"]);
-          next();
-        });
-      })
-      .then(function(next) {
-        $('#js-applists').show();
-      });
-
-  }
+  /*
+   *
+   *  Helper Functions
+   *
+   */
 
   function appendList(target, appList, extraClasses) {
     if(extraClasses) {
@@ -75,21 +59,33 @@
     return data;
   }
 
+  function setIframeHeight(selector) {
+    $(selector).css('height', window.innerHeight - headerHeight);
+  }
+
+  /*
+   *
+   *  Event Handlers
+   *
+   */
+
   function installApp(ev) {
     ev.preventDefault();
     ev.stopPropagation();
     var somewhere = $(this)
       , goButton = '<a class="gobutton" href="http://'+location.hostname+':'+location.port+'/#' 
                   + this.dataset.appname
-                  + '"/>Go!</a>';
+                  + '"/>Go!</a>'
+      , appNameArray = []
       ;
+    appNameArray.push(this.dataset.appname);
     somewhere.removeClass('js-available').addClass('installing').addClass('js-installing');
     request.post('http://localhost:' + port + '/install/'+ this.dataset.appname).when(function (err, ahr, data) {
       data = checkResponse(err, data);
       if(!data) { console.error("bad data!"); return; }
       console.log('installed!', data);
       somewhere.remove();
-      appendList('.js-currently-installed', ['tracker-display'], ['freshly-installed']);
+      appendList('.js-currently-installed', appNameArray, ['freshly-installed', 'js-ready']);
     });
   }
 
@@ -98,12 +94,25 @@
     ev.stopPropagation();
   }
 
-  function assignHandlers() {
-    $('body').delegate('.js-available', 'click', installApp);
-    $('body').delegate('.js-installing', 'click', doNothing);
+  function reloadWindow(ev) {
+    setTimeout(function() {
+      window.location.reload(true);
+    }, 1);
   }
 
-  // Check that appr is installed.
+  function backButton(ev) {
+    
+  }
+
+
+  /*
+   * 
+   *  Fired on domReady(in order)
+   *
+   */
+
+  $.domReady(isInstalled);
+
   function isInstalled() {
     $('#not-installed').hide();
     request.get('http://localhost:' + port + '/alive', null, { timeout: 1000 }).when(function(err, ahr, data) {
@@ -114,27 +123,6 @@
       }
       showApps();
     });
-  }
-  
-  function fillWithApp() {
-    if(location.hash == "") {
-      return false;
-    }
-    var appName = location.hash.substr(1, location.hash.length)
-      , iframeHtml  = '<iframe id="appshown" src="http://localhost:'+port+'/'+appName+'/"'+'></iframe>'
-      ;
-    console.log('iframehtml:', iframeHtml);
-                    
-    $('#container').hide();
-    $('#show-app').empty();
-    $('#show-app').append(iframeHtml);
-    setIframeHeight('#show-app');
-    $('#show-app').show();
-    $('#back_button').show();
-  }
-
-  function setIframeHeight(selector) {
-    $(selector).css('height', window.innerHeight - headerHeight);
   }
 
   function showApps() {
@@ -149,6 +137,55 @@
     });
   }
 
-  $.domReady(isInstalled);
+  function populateLists() {
+    $('#js-applists').hide();
+    sequence
+      .then(function(next) {
+        request.get("http://localhost:" + port + "/installed").when( function(err, ahr, data) {
+          data = checkResponse(err, data);
+          if(!data) { console.error("bad data!"); return; }
+          appendList('.js-currently-installed', data.data, ['js-ready']);
+          next(data.data);
+        });
+      })
+      .then(function(next, alreadyInstalled) {
+        var availableApps
+          ;
+        request.get("http://localhost:" + port + "/applist").when( function(err, ahr, data) {
+          data = checkResponse(err, data);
+          if(!data) { console.error("bad data!"); return; }
+          availableApps = _.difference(data, alreadyInstalled);
+          appendList('.js-app-list', availableApps, ["js-available"]);
+          next();
+        });
+      })
+      .then(function(next) {
+        $('#js-applists').show();
+      });
+  }
+
+  function assignHandlers() {
+    $('body').delegate('.js-available', 'click', installApp);
+    $('body').delegate('.js-installing', 'click', doNothing);
+    $('body').delegate('.js-ready', 'click', reloadWindow);
+    $('body').delegate('#back_button', 'click', backButton);
+  }
+
+  function fillWithApp() {
+    if(location.hash == "") {
+      return false;
+    }
+    var appName = location.hash.substr(1, location.hash.length)
+      , iframeHtml  = '<iframe id="appshown" src="http://localhost:'+port+'/'+appName+'/"'+'></iframe>'
+      ;
+    console.log('iframehtml:', iframeHtml);
+                    
+    $('#container').hide();
+    $('#show-app').empty();
+    $('#show-app').append(iframeHtml);
+    setIframeHeight('#show-app');
+    $('#show-app').show();
+    $('#back_button')[0].style.display='block';
+  }
 
 }());
